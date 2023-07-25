3f6c36011615be2b2479ebf53d13b2ac900b2e47
Yassine Khiadouch <yassine.uav@gmail.com>
2023-07-25 03:01:25 -0700

    review the basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,25 @@
 
 const val name: String = "Nana"
+var greeting: String? = "hello"
 
 fun main() {
+
+}
+
+fun conditions(){
+    if(name != null){
+        print("name isn't null")
+    }else{
+        print("name is null")
+    }
+    println("")
+
+    when(greeting){
+        null -> print("hi")
+        "hello" -> print("$greeting $name")
+        else -> print(greeting)
+    }
     println(name);
+
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
 }
